cd5bddc2cab30945b3cc88ed349a4020e25b52b0
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-03 12:59:52 +0300

    rent & reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(function () {
 
+  $('.top-block__choice-slider').slick({
+    arrows:false,
+    dots:true,
+    autoplay:true,
+    autoplaySpeed: 9000,
+    fade:true,
+  });
+  $('.reviews-content').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:false,
+    dots:true,
+  });
+
+
+
+
 })
